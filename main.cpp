256def58ea010a3eaa468ca91db88e7274e752d9
윤현중 <58295105+HyeonjungYun@users.noreply.github.com>
2024-12-30 02:39:36 +0900

    전체 인터페이스 수정

--- main.cpp
+++ main.cpp
@@ -1,39 +1,62 @@
 #include "Preprocessor.h"
 #include "BookManager.h"
+#include "BorrowManager.h"
+#include "TotalManager.h"
 
 int main() {
-	BookManager bookmanager;
+	BookManager bookManager;
+	BorrowManager borrowManager;
+	TotalManager totalManager(bookManager, borrowManager);
 	while (1) {
-		int selNum;
-		cout << "1.책 신규등록 2. 현재 책 출력 3.제목으로 책 찾기 4.작가이름으로 책 찾기 5.종료" << endl;
-		cin >> selNum;
+		string choice;
+		cout << "1. 책 신규등록" << endl;
+		cout << "2. 책 목록" << endl;
+		cout << "3. 현재 대여 현황" << endl;
+		cout << "4. 책 제목으로 책 찾기" << endl;
+		cout << "5. 작가 이름으로 책 찾기" << endl;
+		cout << "6. 책 이름으로 책 대여" << endl;
+		cout << "7. 작가 이름으로 책 대여" << endl;
+		cout << "8. 종료" << endl;
+		cout << "입력 : ";
+		getline(cin, choice);
+		cout << choice << endl;
 
-		if (selNum == 1) {
+		if (choice == "1") {
 			string title, author;
 			cout << "Title : ";
-			cin >> title;
+			getline(cin, title);
 			cout << "Author : ";
-			cin >> author;
-			bookmanager.addBook(title, author);
+			getline(cin, author);
+			totalManager.addBook(title, author);
 		}
-		if (selNum == 2) bookmanager.displayAllBooks();
-		if (selNum == 3) {
+		if (choice == "2") bookManager.displayAllBooks();
+		if (choice == "3") borrowManager.displayStock();
+		if (choice == "4") {
 			string title;
 			cout << "Title : ";
-			cin >> title;
-			bookmanager.searchByTitle(title);
+			getline(cin, title);
+			bookManager.searchByTitle(title);
 		}
-		if (selNum == 4) {
+		if (choice == "5") {
 			string author;
 			cout << "Author : ";
-			cin >> author;
-			bookmanager.searchByAuthor(author);
+			getline(cin, author);
+			bookManager.searchByAuthor(author);
 		}
-		if (selNum == 5) break;
-		if (!(selNum >= 1 && selNum <= 5)) {
-			cout << "잘못 입력하셨습니다." << endl;
-			getchar();
-			getchar();
+		if (choice == "6") {
+			string title;
+			cout << "Title : ";
+			getline(cin, title);
+
+			borrowManager.borrowBook(title);
 		}
+		if (choice == "7") {
+			string author;
+			cout << "Author : ";
+			getline(cin, author);
+
+			totalManager.borrowBookByAuthor(author);
+		}
+		if (choice == "8") break;
 	}
 }
