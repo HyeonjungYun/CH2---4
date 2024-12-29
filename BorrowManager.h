f9b3a50e80fcbae3bb3f19fa661f5fe6ef370b7a
윤현중 <58295105+HyeonjungYun@users.noreply.github.com>
2024-12-30 01:45:03 +0900

    함수 매개변수 변경 및 파일 분할

--- BorrowManager.h
+++ BorrowManager.h
@@ -6,38 +6,38 @@
 
 class BorrowManager {
 private:
-	unordered_map<Book, int> stock;
+	unordered_map<string, int> stock;
 public:
-	void initializeStock(Book book, int quantity = 3);
-	void borrowBook(Book book);
-	void returnBook(Book book);
+	void initializeStock(string title, int quantity = 3);
+	void borrowBook(string title);
+	void returnBook(string title);
 	void displayStock();
 };
 
-void BorrowManager::initializeStock(Book book, int quantity = 3) {
-	stock.insert(make_pair(book, quantity));
+void BorrowManager::initializeStock(string title, int quantity) {
+	stock.insert(make_pair(title, quantity));
 }
 
-void BorrowManager::borrowBook(Book book) {
-	if (stock[book] == 0) {
+void BorrowManager::borrowBook(string title) {
+	if (stock[title] == 0) {
 		cout << "대여 가능한 책이 없습니다." << endl;
 		return;
 	}
-	stock[book]--;
+	stock[title] -= 1;
 }
 
-void BorrowManager::returnBook(Book book) {
-	if (stock[book] == 3) {
+void BorrowManager::returnBook(string title) {
+	if (stock[title] == 3) {
 		cout << "대여된 책이 없습니다." << endl;
 		return;
 	}
-	stock[book]++;
+	stock[title] += 1;
 }
 
 void BorrowManager::displayStock() {
 	cout << "현재 책 대여 상황" << endl;
-	for (pair<Book, int> element : stock)
-		cout << element.first.getTitle() << " By " << element.first.getAuthor() << ", 남은 책 : " << element.second << endl;
+	for (pair<string, int> element : stock)
+		cout << element.first << ", 남은 책 : " << element.second << endl;
 }
 
 #endif
